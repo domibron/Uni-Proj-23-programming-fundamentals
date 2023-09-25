0bf46f175d90c437b69268c55cec692a71779c14
dominic <dommcneill@outlook.com>
2023-09-25 19:48:06 +0100

    created c# project

---
+++ Project/Q102061800_McNeill_Dominic_DAC416_AE1_Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Q102061800_McNeill_Dominic_DAC416_AE1_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

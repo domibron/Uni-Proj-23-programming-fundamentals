d746d598f9dd91537f4ce9672cb0eb570dc5ea4a
dominic <dommcneill@outlook.com>
2023-11-14 18:32:42 +0000

    some work

--- Project/Q102061800_McNeill_Dominic_DAC416_AE1_Project/ConsoleApp/Program.cs
+++ Project/Q102061800_McNeill_Dominic_DAC416_AE1_Project/ConsoleApp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using RougeGame;
 
 namespace ConsoleApp
@@ -10,28 +9,7 @@ namespace ConsoleApp
         {
             //RougeGameTest.TestAll();
 
-            int[,] image =
-            {
-                {0,4,4,4,0,0},
-                {4,4,11,11,0,0},
-                {4,4,4,4,0,0},
-                {0,4,0,4,0,0},
-                {0,4,0,4,0,0},
-                {0,4,0,4,0,0},
-                {0,7,6,7,6,0},
-                {0,7,7,8,7,8}
-            };
-
-            for (int x = 0; x < image.GetLength(0); x++)
-            {
-                for (int y = 0; y < image.GetLength(1); y++)
-                {
-                    Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), (string)Enum.GetName(typeof(ConsoleColor), image[x, y]));
-                    Console.Write("██");
-
-                }
-                Console.Write("\n");
-            }
+            
 
             RougeGameCore.GameCore();
         }
---
+++ Project/Q102061800_McNeill_Dominic_DAC416_AE1_Project/ConsoleApp/RougeGameDrawImage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RougeGame
+{
+    public class RougeGameDrawImage
+    {
+        public static void DrawImageTest()
+        {
+            int[,] image =
+                {
+                {0,4,4,4,0,0},
+                {4,4,11,11,0,0},
+                {4,4,4,4,0,0},
+                {0,4,0,4,0,0},
+                {0,4,0,4,0,0},
+                {0,4,0,4,0,0},
+                {0,7,6,7,6,0},
+                {0,7,7,8,7,8}
+            };
+
+            for (int x = 0; x < image.GetLength(0); x++)
+            {
+                for (int y = 0; y < image.GetLength(1); y++)
+                {
+                    Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), Enum.GetName(typeof(ConsoleColor), image[x, y]));
+                    Console.Write("██");
+
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}

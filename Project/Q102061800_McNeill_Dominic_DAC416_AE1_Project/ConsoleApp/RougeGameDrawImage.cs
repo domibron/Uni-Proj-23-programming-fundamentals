c00ba86c71ff9d4a8a499b8a1ddcf9595bd1d0ea
dominic <dommcneill@outlook.com>
2023-11-14 18:36:58 +0000

    some more work

--- Project/Q102061800_McNeill_Dominic_DAC416_AE1_Project/ConsoleApp/RougeGameDrawImage.cs
+++ Project/Q102061800_McNeill_Dominic_DAC416_AE1_Project/ConsoleApp/RougeGameDrawImage.cs
@@ -8,25 +8,25 @@ namespace RougeGame
 {
     public class RougeGameDrawImage
     {
-        public static void DrawImageTest()
+        public static void DrawImageTest(int[,] imageMatrix)
         {
-            int[,] image =
-                {
-                {0,4,4,4,0,0},
-                {4,4,11,11,0,0},
-                {4,4,4,4,0,0},
-                {0,4,0,4,0,0},
-                {0,4,0,4,0,0},
-                {0,4,0,4,0,0},
-                {0,7,6,7,6,0},
-                {0,7,7,8,7,8}
-            };
+            //int[,] image =
+            //    {
+            //    {0,4,4,4,0,0},
+            //    {4,4,11,11,0,0},
+            //    {4,4,4,4,0,0},
+            //    {0,4,0,4,0,0},
+            //    {0,4,0,4,0,0},
+            //    {0,4,0,4,0,0},
+            //    {0,7,6,7,6,0},
+            //    {0,7,7,8,7,8}
+            //};
 
-            for (int x = 0; x < image.GetLength(0); x++)
+            for (int x = 0; x < imageMatrix.GetLength(0); x++)
             {
-                for (int y = 0; y < image.GetLength(1); y++)
+                for (int y = 0; y < imageMatrix.GetLength(1); y++)
                 {
-                    Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), Enum.GetName(typeof(ConsoleColor), image[x, y]));
+                    Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), Enum.GetName(typeof(ConsoleColor), imageMatrix[x, y]));
                     Console.Write("██");
 
                 }
